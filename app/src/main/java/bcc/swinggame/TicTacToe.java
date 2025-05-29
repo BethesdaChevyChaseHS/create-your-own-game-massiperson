bc3f386d7b63b36cca93445d45b5a86226ffff98
massiperson <max7trea@gmail.com>
2025-05-29 12:16:18 +0000

    submitting

---
+++ app/src/main/java/bcc/swinggame/TicTacToe.java
@@ -0,0 +1,14 @@
+public class TicTacToe {
+    public static void main(String[] args)
+    {
+        int squaresPlayed = 0;
+        String turn = "x";
+        String[] board = {" ", " ", " ", " ", " ", " ", " ", " ", " "};
+
+        public void printTheBoard(String[] b)
+        {
+
+        }
+    }
+    
+}
